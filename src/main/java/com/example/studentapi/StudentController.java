15029758c86680ffba67e5048862767dca1cc036
Rahul <adihetti_im17003@kln.stu.ac.lk>
2021-05-15 23:55:52 +0530

    Adding Endpoints

---
+++ src/main/java/com/example/studentapi/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.studentapi;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentController {
+
+    StudentRepository repository = new InMemoryStudentRepository();
+
+    //REST ENDPOINTS
+
+    //crate a student
+
+    @PostMapping("/student")
+    public void createStudent(@RequestBody Student student){
+
+        repository.save(student);
+
+    }
+
+    //retrieve a student
+
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable String id){
+
+        return repository.get(id);
+    }
+
+
+    //delete a student
+    @DeleteMapping("/student/{id}")
+
+    public void deleteStudent(@PathVariable String id){
+
+        repository.delete(id);
+
+    }
+
+    //update a student
+
+}
